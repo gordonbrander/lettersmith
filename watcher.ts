8171a6889f61ec9eeca2a6bfbc3a2fab2251a638
Gordon Brander <gordonbrander@users.noreply.github.com>
2025-06-06 09:08:13 -0400

    Run watcher in separate async fn, returning cancel

--- watcher.ts
+++ watcher.ts
@@ -2,26 +2,33 @@ import { debounce } from "@std/async";
 import { globToRegExp } from "@std/path";
 
 /** Exclude paths that match the given regular expression */
-export const excludePaths = (paths: string[], exclude: RegExp) =>
+export const excludePaths = (paths: string[], exclude: RegExp): string[] =>
   paths.filter((p) => !exclude.test(p));
 
 /** Watch paths for changes, calling a callback on change.
  * @param path - The file or directory path to watch for changes
  * @param exclude - A glob pattern string to exclude matching paths from triggering the callback
  * @param callback - Function to call when changes are detected to non-excluded paths
+ * @returns A function to close the watcher
  */
-export const watch = async (
+export const watch = (
   path: string,
   exclude: string,
   callback: () => unknown,
-) => {
+): () => void => {
   const watcher = Deno.watchFs(path);
   const debouncedCallback = debounce(callback, 100);
   const excludeRegex = globToRegExp(exclude);
-  for await (const event of watcher) {
-    const changed = excludePaths(event.paths, excludeRegex);
-    if (changed.length > 0) {
-      debouncedCallback();
+
+  const watch = async () => {
+    for await (const event of watcher) {
+      const changed = excludePaths(event.paths, excludeRegex);
+      if (changed.length > 0) {
+        debouncedCallback();
+      }
     }
-  }
+  };
+  watch();
+
+  return () => watcher.close();
 };
